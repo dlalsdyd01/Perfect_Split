802a77c650cc5d80c909865e1d8bd58d0bf98304
Leeminyong <kv0435029@naver.com>
2026-04-23 00:49:48 +0900

    Initial Commit

---
+++ Perfect_Split/Perfect_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Perfect_SplitApp.swift
+//  Perfect_Split
+//
+//  Created by 이민용 on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Perfect_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
